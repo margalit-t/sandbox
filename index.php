2935817326c97bc10531179e5593b57d649b474f
margalit-t <132431154+margalit-t@users.noreply.github.com>
2023-06-29 14:06:29 +0300

    new project

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//console app
+
+echo "margalit&ester";
+
+for ($i = 0; $i < 10; $i++) {
+    echo $i . "<br/>";
+}
+
+?>
+
+<h1>Hello! itws new</h1>
+
+//
+//$n = intval(trim(fgets(STDIN)));
+//for ($i = 0; $i < $n; $i++) {
+//    echo $i . PHP_EOL;
+//}
+
+
+// web app
+// debug method 1: using web application settings
+//echo "margalit&ester";
+
+
